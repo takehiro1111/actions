1898576852cf25bb5c86122a1f874a0baf86a9b1
Takehiro Tanaka <t.soccer.0916@gmail.com>
2023-12-30 14:35:07 +0900

    保存

--- compute.tf
+++ compute.tf
@@ -19,3 +19,53 @@ resource "aws_key_pair" "public_instance_1" {
   key_name   = "actions"
   public_key = file("./key/actions.pub")
 }
+
+resource "aws_instance" "count" {
+  count = 2
+  ami = "ami-0dafcef159a1fc745" // Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
+  subnet_id = aws_subnet.public_a.id
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.public_instance_1.key_name
+  vpc_security_group_ids = [aws_security_group.ec2_public.id]
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_type           = "gp3"
+    volume_size           = 8
+    delete_on_termination = true
+    encrypted             = true
+
+    tags = {
+      Name = "ebs-actions"
+    }
+  }
+
+ tags = {
+    Name = "server${count.index}"
+ }
+}
+
+resource "aws_instance" "for_each" {
+  for_each = toset(local.family)
+  ami = "ami-0dafcef159a1fc745" // Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
+  subnet_id = aws_subnet.public_a.id
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.public_instance_1.key_name
+  vpc_security_group_ids = [aws_security_group.ec2_public.id]
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_type           = "gp3"
+    volume_size           = 8
+    delete_on_termination = true
+    encrypted             = true
+
+    tags = {
+      Name = "ebs-actions"
+    }
+  }
+
+ tags = {
+    Name = "server${each.value}"
+ }
+}
--- local.tf
+++ local.tf
@@ -7,4 +7,23 @@ locals {
   //自宅用IP
   current-ip = chomp(data.http.ifconfig.body)
   my_ip      = (var.my_ipv4 == null) ? "${local.current-ip}/32" : var.my_ipv4
+
+  //for_each
+  family = [
+    "ayumi",
+    "yasuhiro",
+    "chisato",
+    "takehiro",
+  ]
+
+  //try
+  try_test = [
+    {
+    name = "try1",
+   // description = "try-first"
+    },
+    {
+    name = "try2"
+    },
+  ] 
 }
